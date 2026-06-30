65e1e65d81ad58bdd254440f77856b658d4f430d
gabrielesteban0413 <gabrielesteban0413@gmail.com>
2026-06-29 22:56:29 -0500

    stock-products

--- backend/prisma/migrations/20260603042015_init/migration.sql
+++ backend/prisma/migrations/20260603042015_init/migration.sql
@@ -1,13 +1,15 @@
 -- CreateTable
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "solution" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
     "imageUrl" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "solution" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    
+
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
